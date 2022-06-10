8da1df0332cc3f47ff3805b38638e8cd0373295e
Воронцов Илья Викторович <Nebmes@mail.ru>
2022-06-10 21:41:14 +0300

    add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+string numberInt = Console.ReadLine();
+int number = int.Parse(numberInt);
+int i = 1;
+while (i<=number)
+{
+    if (i%2==0)
+    {
+        Console.Write($"{i} ");
+    }
+    i++;
+}
+
+// или
+
+Console.WriteLine();
+Console.Write("Введите число: ");
+string numberStr = Console.ReadLine();
+int num = int.Parse(numberStr);
+int ind = 2;
+while (ind <= num)
+{
+    Console.Write($"{ind} ");
+    ind += 2;
+}
